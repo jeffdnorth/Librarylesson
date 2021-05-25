10e764396dc4c0f77c17c009c0b71353ab8d1318
Jeff North <jnorth@tql.com>
2021-05-25 12:40:22 -0400

    Add project files.

---
+++ Librarylesson/MathLib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Librarylesson
+{
+    public class MathLib
+    {
+        //create methods  all static so we can call directly
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Mult(int a, int b)
+            {
+            return a * b;
+            }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+            public static   int Modulo(int a,int b)
+        {
+            return Sub(a, Mult(Div(a, b), b));
+        }
+
+    }
+}
---
+++ TestLibraryLeson/Program.cs
@@ -0,0 +1,20 @@
+﻿using Librarylesson;
+using System;
+
+namespace TestLibraryLeson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //when you have static methods do not need to creat instance of class instead use
+            // class name.method name
+            var OnePlusTwo = MathLib.Add(1, 2);
+            var ans = MathLib.Modulo(5, 3);
+        }
+        //method
+        
+        
+       
+    }
+}
